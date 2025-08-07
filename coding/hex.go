044a02a53e720d47349328a1070f281803a3abaf
gouguoyin <245629560@qq.com>
2025-08-08 00:02:45 +0800

    feat(coding): Refactor encoding and decoding for hex, and added streaming support

---
+++ coding/hex.go
@@ -0,0 +1,41 @@
+package coding
+
+import (
+	"io"
+
+	"github.com/dromara/dongle/coding/hex"
+)
+
+// ByHex encodes by hex.
+func (e *Encoder) ByHex() *Encoder {
+	if e.Error != nil {
+		return e
+	}
+	if e.reader != nil {
+		e.dst, e.Error = e.stream(func(w io.Writer) io.WriteCloser {
+			return hex.NewStreamEncoder(w)
+		})
+		return e
+	}
+	if len(e.src) > 0 {
+		e.dst = hex.NewStdEncoder().Encode(e.src)
+	}
+	return e
+}
+
+// ByHex decodes by hex.
+func (d *Decoder) ByHex() *Decoder {
+	if d.Error != nil {
+		return d
+	}
+	if d.reader != nil {
+		d.dst, d.Error = d.stream(func(r io.Reader) io.Reader {
+			return hex.NewStreamDecoder(r)
+		})
+		return d
+	}
+	if len(d.src) > 0 {
+		d.dst, d.Error = hex.NewStdDecoder().Decode(d.src)
+	}
+	return d
+}
