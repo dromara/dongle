e556a276964054a4adf1e761790c29eb64255ca2
gouguoyin <245629560@qq.com>
2025-08-08 00:01:17 +0800

    feat(coding): Refactor encoding and decoding for morse, and added streaming support

---
+++ coding/morse.go
@@ -0,0 +1,51 @@
+package coding
+
+import (
+	"io"
+
+	"github.com/dromara/dongle/coding/morse"
+)
+
+// ByMorse encodes by morse code.
+func (e *Encoder) ByMorse() *Encoder {
+	if e.Error != nil {
+		return e
+	}
+
+	if e.reader != nil {
+		e.dst, e.Error = e.stream(func(w io.Writer) io.WriteCloser {
+			return morse.NewStreamEncoder(w)
+		})
+		return e
+	}
+
+	if len(e.src) == 0 {
+		return e
+	}
+	encoder := morse.NewStdEncoder()
+	e.dst = encoder.Encode(e.src)
+	if encoder.Error != nil {
+		e.Error = encoder.Error
+	}
+	return e
+}
+
+// ByMorse decodes by morse code.
+func (d *Decoder) ByMorse() *Decoder {
+	if d.Error != nil {
+		return d
+	}
+
+	if d.reader != nil {
+		d.dst, d.Error = d.stream(func(r io.Reader) io.Reader {
+			return morse.NewStreamDecoder(r)
+		})
+		return d
+	}
+
+	if len(d.src) == 0 {
+		return d
+	}
+	d.dst, d.Error = morse.NewStdDecoder().Decode(d.src)
+	return d
+}
