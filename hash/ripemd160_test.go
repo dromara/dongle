57a68faaa3ceabe978bddf07e0f0e2f4b53edffb
gouguoyin <245629560@qq.com>
2025-09-14 20:31:26 +0800

    refactor(hash): Unify import package order

--- hash/ripemd160_test.go
+++ hash/ripemd160_test.go
@@ -5,9 +5,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/dromara/dongle/mock"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHasher_ByRipemd160(t *testing.T) {
