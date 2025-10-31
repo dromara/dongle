0893b5d553e5d39d7e02298b2cf9c74333157b21
gouguoyin <245629560@qq.com>
2025-10-31 20:01:49 +0800

    feat(keypair): Add formatKeyBody function

--- crypto/keypair/keypair.go
+++ crypto/keypair/keypair.go
@@ -3,6 +3,10 @@
 // cryptographic algorithms with different key formats.
 package keypair
 
+import (
+	"github.com/dromara/dongle/utils"
+)
+
 // KeyFormat represents the format of RSA keys.
 // It can be either PKCS1 or PKCS8 format.
 type KeyFormat string
@@ -17,3 +21,21 @@ const (
 	// This format uses generic headers like "-----BEGIN PUBLIC KEY-----".
 	PKCS8 KeyFormat = "pkcs8"
 )
+
+// formatKeyBody formats the key body into 64-character lines with the specified header and tail.
+// This is a helper function used by formatPublicKey and formatPrivateKey.
+func formatKeyBody(keyBody []byte, header, tail string) []byte {
+	bodyStr := utils.Bytes2String(keyBody)
+	formatted := header
+
+	// Split the key body into 64-character lines
+	for i := 0; i < len(bodyStr); i += 64 {
+		end := i + 64
+		if end > len(bodyStr) {
+			end = len(bodyStr)
+		}
+		formatted += bodyStr[i:end] + "\n"
+	}
+	formatted += tail
+	return utils.String2Bytes(formatted)
+}
