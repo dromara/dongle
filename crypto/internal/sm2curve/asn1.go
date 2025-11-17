bf9353d4f06e3b3d67cb43588aea45c403bcbab8
WebChang <web-chang@foxmail.com>
2025-11-17 08:32:13 +0000

    add parse BIT_STRING as SM2 key:   crypto/internal/sm2curve/asn1.go

--- crypto/internal/sm2curve/asn1.go
+++ crypto/internal/sm2curve/asn1.go
@@ -68,7 +68,7 @@ func MarshalPKCS8PrivateKey(pri *ecdsa.PrivateKey) ([]byte, error) {
 	return p8.Bytes()
 }
 
-// ParseDerPublicKey parses a BIT STRING and returns an SM2 public key.
+// ParseDerPublicKey parses a BIT_STRING and returns an SM2 public key.
 //
 //go:inline
 func ParseDerPublicKey(der []byte) (*ecdsa.PublicKey, error) {
@@ -105,7 +105,7 @@ func ParseSPKIPublicKey(der []byte) (*ecdsa.PublicKey, error) {
 	return ParseDerPublicKey(point)
 }
 
-// ParseDerPrivateKey parses a BIT STRING PrivateKeyInfo and returns an SM2 private key.
+// ParseDerPrivateKey parses a BIT_STRING PrivateKeyInfo and returns an SM2 private key.
 //
 //go:inline
 func ParseDerPrivateKey(der []byte) (*ecdsa.PrivateKey, error) {
