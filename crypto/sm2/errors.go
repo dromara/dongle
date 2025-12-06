7bc33cd9be046112be1e5f31eac394fd61ecea91
gouguoyin <245629560@qq.com>
2025-12-06 22:48:45 +0800

    refactor(crypto/sm2): Remove unused error types

--- crypto/sm2/errors.go
+++ crypto/sm2/errors.go
@@ -2,80 +2,40 @@ package sm2
 
 import "fmt"
 
-type NilKeyPairError struct{}
-
-func (e NilKeyPairError) Error() string {
-	return fmt.Sprintf("key pair cannot be nil")
+type EncryptError struct {
+	Err error
 }
 
-type PublicKeyUnsetError struct{}
-
-func (e PublicKeyUnsetError) Error() string {
-	return fmt.Sprintf("public key not set, please use SetPublicKey() method")
+func (e EncryptError) Error() string {
+	return fmt.Sprintf("crypto/sm2: failed to encrypt data: %v", e.Err)
 }
 
-type PrivateKeyUnsetError struct{}
-
-func (e PrivateKeyUnsetError) Error() string {
-	return fmt.Sprintf("private key not set, please use SetPrivateKey() method")
+type DecryptError struct {
+	Err error
 }
 
-type KeyPairError struct{ Err error }
-
-func (e KeyPairError) Error() string {
-	if e.Err == nil {
-		return fmt.Sprintf("invalid key pair")
-	}
-	return fmt.Sprintf("invalid key pair: %v", e.Err)
-}
-
-type SignError struct{ Err error }
-
-func (e SignError) Error() string {
-	if e.Err == nil {
-		return fmt.Sprintf("sign error")
-	}
-	return fmt.Sprintf("sign error: %v", e.Err)
-}
-
-type VerifyError struct{ Err error }
-
-func (e VerifyError) Error() string {
-	if e.Err == nil {
-		return fmt.Sprintf("verify error")
-	}
-	return fmt.Sprintf("verify error: %v", e.Err)
+func (e DecryptError) Error() string {
+	return fmt.Sprintf("crypto/sm2: failed to decrypt data: %v", e.Err)
 }
 
 type ReadError struct{ Err error }
 
 func (e ReadError) Error() string {
-	if e.Err == nil {
-		return fmt.Sprintf("read error")
-	}
-	return fmt.Sprintf("read error: %v", e.Err)
+	return fmt.Sprintf("crypto/sm2: failed to read encrypted data: %v", e.Err)
 }
 
-type NoSignatureError struct{}
-
-func (e NoSignatureError) Error() string {
-	return fmt.Sprintf("crypto/sm2: no signature provided for verification")
+type SignError struct {
+	Err error
 }
 
-type EncryptError struct{ Err error }
-
-func (e EncryptError) Error() string {
-	if e.Err == nil {
-		return fmt.Sprintf("encrypt error")
-	}
-	return fmt.Sprintf("encrypt error: %v", e.Err)
+func (e SignError) Error() string {
+	return fmt.Sprintf("crypto/sm2: failed to sign data: %v", e.Err)
 }
 
-type DecryptError struct{ Err error }
+type VerifyError struct {
+	Err error
+}
 
-func (e DecryptError) Error() string {
-	if e.Err == nil {
-		return fmt.Sprintf("decrypt error")
-	}
-	return fmt.Sprintf("decrypt error: %v", e.Err)
+func (e VerifyError) Error() string {
+	return fmt.Sprintf("crypto/sm2: failed to verify signature: %v", e.Err)
 }
