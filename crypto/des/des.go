e66e50862f76ee7853576ed104028ecf865e4618
gouguoyin <245629560@qq.com>
2025-12-06 22:37:35 +0800

    refactor(crypto/des): decrypted -> buffer, pos -> position

--- crypto/des/des.go
+++ crypto/des/des.go
@@ -218,12 +218,12 @@ func (e *StreamEncrypter) Close() error {
 // It implements DES decryption using the standard DES algorithm with support
 // for 64-bit keys and various cipher modes, providing streaming capabilities with proper state management.
 type StreamDecrypter struct {
-	reader    io.Reader         // Underlying reader for encrypted input
-	cipher    *cipher.DesCipher // The cipher interface for decryption operations
-	decrypted []byte            // All decrypted data
-	pos       int               // Current position in the decrypted data
-	block     stdCipher.Block   // Reused cipher block for better performance
-	Error     error             // Error field for storing decryption errors
+	reader   io.Reader         // Underlying reader for encrypted input
+	cipher   *cipher.DesCipher // The cipher interface for decryption operations
+	buffer   []byte            // Buffer for decrypted data
+	position int               // Current position in the buffer
+	block    stdCipher.Block   // Reused cipher block for better performance
+	Error    error             // Error field for storing decryption errors
 }
 
 // NewStreamDecrypter creates a new DES stream decrypter with the specified reader and cipher.
@@ -232,10 +232,10 @@ type StreamDecrypter struct {
 // Only CBC, CTR, ECB, CFB, and OFB modes are supported.
 func NewStreamDecrypter(r io.Reader, c *cipher.DesCipher) io.Reader {
 	d := &StreamDecrypter{
-		reader:    r,
-		cipher:    c,
-		decrypted: nil, // Will be populated on first read
-		pos:       0,
+		reader:   r,
+		cipher:   c,
+		buffer:   nil, // Will be populated on first read
+		position: 0,
 	}
 
 	if len(c.Key) != 8 {
@@ -263,7 +263,7 @@ func (d *StreamDecrypter) Read(p []byte) (n int, err error) {
 	}
 
 	// If we haven't decrypted the data yet, do it now
-	if d.decrypted == nil {
+	if d.buffer == nil {
 		// Read all encrypted data from the underlying reader
 		encryptedData, err := io.ReadAll(d.reader)
 		if err != nil {
@@ -290,19 +290,19 @@ func (d *StreamDecrypter) Read(p []byte) (n int, err error) {
 			return 0, DecryptError{Err: err}
 		}
 
-		d.decrypted = decrypted
-		d.pos = 0
+		d.buffer = decrypted
+		d.position = 0
 	}
 
 	// If we've already returned all decrypted data, return EOF
-	if d.pos >= len(d.decrypted) {
+	if d.position >= len(d.buffer) {
 		return 0, io.EOF
 	}
 
 	// Copy as much decrypted data as possible to the provided buffer
-	remainingData := d.decrypted[d.pos:]
+	remainingData := d.buffer[d.position:]
 	copied := copy(p, remainingData)
-	d.pos += copied
+	d.position += copied
 
 	return copied, nil
 }
